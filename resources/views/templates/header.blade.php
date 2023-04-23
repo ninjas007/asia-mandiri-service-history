f22fe5280509c91ea71c6a85acd03f9164afb38b
adi <tilistiadi@getslurp.com>
2023-04-23 17:42:21 +0700

    fix: hapus salah tulis

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,6 +1,5 @@
 <div class="body-header">
     <div class="body-header-content">
-        @endphp
         App CV Asia Mandiri - Login Sebagai {{ auth()->user()->role_id == 0 ? 'Adin' }}
     </div>
 </div>
