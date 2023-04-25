b8ceb75ee59be24e1986959b7d203032cf854153
adi <tilistiadi@getslurp.com>
2023-04-25 15:29:10 +0700

    fix: handle auth user

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,5 +1,9 @@
 <div class="body-header">
     <div class="body-header-content">
+        @if (auth()->user() ?? false)
         App CV Asia Mandiri - Login Sebagai {{ auth()->user()->role_id == 0 ? 'Admin' : 'User' }}
+        @else
+        App CV Asia Mandiri
+        @endif
     </div>
 </div>
