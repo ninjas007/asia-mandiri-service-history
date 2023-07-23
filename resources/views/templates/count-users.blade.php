28bbde66170e3400a7dbc6b86a7e158e4524be72
adi <tilis.tiadi@silentmode.my>
2023-07-23 14:51:55 +0700

    chore: col6 count users

--- resources/views/templates/count-users.blade.php
+++ resources/views/templates/count-users.blade.php
@@ -1,12 +1,12 @@
 @if (auth()->user()->role_id == 0 && request()->detail != 1 && Request::segment(1) == 'akun')
     <div class="row">
-        <a href="{{ url('akun/teknisi') }}" class="col-md-6 text-center mb-3">
+        <a href="{{ url('akun/teknisi') }}" class="col-6 text-center mb-3">
             <div class="form-group border p-3 {{ $page == 'teknisi' ? 'bg-dark text-white' : 'text-dark' }}">
                 <i class="fa fa-gears" style="color: ; font-size: 4rem"></i>
                 <div class="mt-3 font-weight-bold">Teknisi : <span id="total-teknisi">{{ $total_teknisi }}</span></div>
             </div>
         </a>
-        <a href="{{ url('akun/client') }}" class="col-md-6 text-center mb-3">
+        <a href="{{ url('akun/client') }}" class="col-6 text-center mb-3">
             <div class="form-group border p-3 {{ $page == 'client' ? 'bg-dark text-white' : 'text-dark' }}">
                 <i class="fa fa-users" style="color: ; font-size: 4rem"></i>
                 <div class="mt-3 font-weight-bold">Clients : <span id="total-client">{{ $total_client }}</span></div>
